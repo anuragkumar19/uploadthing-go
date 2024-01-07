aaf76e479d12691673e4edb2d5b3e17df1170465
Anurag Kumar <abhisunny282@gmail.com>
2024-01-07 20:57:08 +0530

    shift dev env to github codespaces

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anuragkumar19/uploadthing-go
+
+go 1.21.5
---
+++ uploadthing.go
@@ -0,0 +1,37 @@
+package uploadthing
+
+import "os"
+
+type Uploadthing struct {
+	appID  string
+	secret string
+}
+
+type UploadthingConfig struct{}
+
+func New() *Uploadthing {
+	appIdEnvKey := "UPLOADTHING_APP_ID"
+	secretEnvKey := "UPLOADTHING_APP_ID"
+
+	appId := os.Getenv(appIdEnvKey)
+	secret := os.Getenv(secretEnvKey)
+
+	if appId == "" {
+		panic("uploadthing: failed to load app id")
+	}
+
+	if secret == "" {
+		panic("uploadthing: failed to load secret")
+	}
+
+	ut := Uploadthing{
+		appID:  appId,
+		secret: secret,
+	}
+
+	return &ut
+}
+
+func NewWithConfig(config *UploadthingConfig) *Uploadthing {
+
+}
